693a226ddfa8e8d6d0f9b6a29bb1034e37023273
Ryan Zhao <rzhao42@icloud.com>
2023-06-04 11:43:43 -0400

    set up eslint and auto lint on save for vscode

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { type Express, type Request, type Response } from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT ?? "8000" ;
+const app: Express = express()
+const port = process.env.PORT ?? '8000'
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server new value');
-});
+  res.send('Express + TypeScript Server new value')
+})
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
