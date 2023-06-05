6ec8a192e2ba4e0a290df0e9bf2443ff2b7d5b44
Ryan Zhao <rzhao42@icloud.com>
2023-06-05 11:33:21 -0400

    remove conflict in eslintrc

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['../.eslintrc.js'],
+  extends: ['standard-with-typescript', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
