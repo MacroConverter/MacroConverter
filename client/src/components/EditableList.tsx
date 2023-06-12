850c469ae4403eb44226a43e756d938996ee0754
Ryan Zhao <rzhao42@icloud.com>
2023-06-12 11:27:25 -0400

    basic list and story

--- client/src/components/EditableList.tsx
+++ client/src/components/EditableList.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../styles/EditableList.css';
 
 interface Ingredient {
   name: string;
+  amount: number;
+  unit: string;
   protein: number;
   fat: number;
   carbs: number;
@@ -19,21 +25,52 @@ const EditableList = ({
 }: AppProps): React.ReactElement => {
   const ingList = ingredients.map((item) => {
     return (
-      <li key={item.name} style={{ display: 'flex' }}>
+      <li key={item.name} className="flex-li">
         <p>{item.name}</p>
-        <button
+        <p>{item.fat}</p>
+        <p>{item.carbs}</p>
+        <p>{item.protein}</p>
+        <p>{item.calories}</p>
+        <p>
+          {item.amount}
+          {item.unit}
+        </p>
+        <div
+          className="btn btn-primary"
           onClick={(): void => {
             onClick();
           }}
         >
           -
-        </button>
+        </div>
       </li>
     );
   });
   return (
     <div>
-      <ul>{ingList}</ul>
+      <ul
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: 'black solid 1px',
+        }}
+      >
+        <li
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <p>name</p>
+          <p>fat</p>
+          <p>carbs</p>
+          <p>protein</p>
+          <p>calories</p>
+          <p>amount</p>
+        </li>
+        {ingList}
+      </ul>
     </div>
   );
 };
--- client/src/components/Editablelist.stories.tsx
+++ client/src/components/Editablelist.stories.tsx
@@ -8,6 +8,25 @@ export default {
 
 export const Default = {
   args: {
-    ingredients: [{ name: 'test' }, { name: 'test2' }],
+    ingredients: [
+      {
+        name: 'test',
+        fat: 12,
+        protein: 12,
+        carb: 12,
+        calories: 100,
+        amount: 123,
+        unit: 'g',
+      },
+      {
+        name: 'test2',
+        fat: 11232,
+        protein: 122,
+        carb: 132,
+        calories: 1100,
+        amount: 1223,
+        unit: 'g',
+      },
+    ],
   },
 };
